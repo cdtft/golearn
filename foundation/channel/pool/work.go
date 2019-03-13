21e4be35102e83ca7f14e54084ef4d4069fd41ea
wangcheng <453451180@qq.com>
2019-03-13 09:32:29 +0800

    :construction: worker

---
+++ foundation/channel/pool/work.go
@@ -0,0 +1,33 @@
+package pool
+
+import "sync"
+
+type Worker interface {
+	Task()
+}
+
+type WorkPool struct {
+	work chan Worker
+	wg   sync.WaitGroup
+}
+
+func NewWorkPool(maxGoroutines int) *WorkPool {
+	p := &WorkPool{
+		work: make(chan Worker),
+	}
+	p.wg.Add(maxGoroutines)
+
+	for i := 0; i < maxGoroutines; i++ {
+		go func() {
+			for work := range p.work {
+				work.Task()
+			}
+			p.wg.Done()
+		}()
+	}
+	return p
+}
+
+func (p *WorkPool) WorkPoolRun(w Worker) {
+	p.work <-w
+}
