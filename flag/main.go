02b69240cd78cd9db5cc9607ea576120e24fe85c
wangcheng <453451180@qq.com>
2019-03-15 10:07:24 +0800

    :ok_hand: flag使用

---
+++ flag/main.go
@@ -0,0 +1,17 @@
+package flag
+
+import (
+	"flag"
+	"fmt"
+)
+
+func init() {
+	flag.StringVar(&name, "name", "everyone", "the greeting object.")
+}
+
+var name string
+
+func main() {
+	flag.Parse()
+	fmt.Printf("hello %s", name)
+}
