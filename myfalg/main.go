1d0a44420b77de0ef35510c23bb88dd23b498917
wangcheng <453451180@qq.com>
2019-03-23 14:47:45 +0800

    :art: test modify data

--- myfalg/main.go
+++ myfalg/main.go
@@ -17,9 +17,21 @@ func main() {
 	var myMap map[string]int
 
 	delete(myMap, "1")
-	myMap["s"] = 1
+	//myMap["s"] = 1
 
 	flag.Parse()
 	fmt.Printf("hello %s \n", name)
 	internal.Hello()
+
+	complexArray := [2][]string{
+		[]string{"d", "e", "f"},
+		[]string{"2", "s", "d"},
+	}
+	modify(complexArray[:])
+
+	fmt.Println(complexArray)
+}
+
+func modify(array [][]string) {
+	fmt.Print(array)
 }
