7706a88d226a90f1fb5de5c6a79fdd3cf12ffed0
wangcheng <453451180@qq.com>
2019-03-29 14:16:48 +0800

    :art: 通道关闭广播

--- select/cancel/cancel_test.go
+++ select/cancel/cancel_test.go
@@ -1,1 +1,43 @@
 package cancel
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestChannel(t *testing.T) {
+
+	cancelChan := make(chan struct{}, 0)
+
+	for i := 0; i < 5; i++ {
+		go func(i int, cancelChan chan struct{}) {
+			for {
+				if isCancelled(cancelChan) {
+					break
+				}
+				time.Sleep(time.Microsecond * 5)
+			}
+			fmt.Println(i, "cancelled")
+		}(i, cancelChan)
+	}
+	time.Sleep(time.Second * 1)
+	closeChannel(cancelChan)
+}
+
+func isCancelled(cancelChan chan struct{}) bool {
+	select {
+	case <-cancelChan:
+		return true
+	default:
+		return false
+	}
+}
+
+func cancel(cancelChan chan struct{}) {
+	cancelChan<- struct{}{}
+}
+
+func closeChannel(cancelChan chan struct{}) {
+	close(cancelChan)
+}
