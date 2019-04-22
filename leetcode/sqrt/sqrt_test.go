b8fe4cf7609879031f941e982b1a1a9ff5ecb7f7
wangcheng <453451180@qq.com>
2019-04-22 13:52:23 +0800

    :zap: 两种递归

--- leetcode/sqrt/sqrt_test.go
+++ leetcode/sqrt/sqrt_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+//逼近法计算开平方
 func TestSqrt(t *testing.T) {
 
 	var x float32 = 2
@@ -19,3 +20,27 @@ func sqrtIter(x float32, y float32) float32 {
 	z := x / y
 	return (z + y) / 2
 }
+
+func TestFactorial(t *testing.T) {
+	fmt.Println(factorial(5))
+	fmt.Println(factorialIter1(5))
+}
+
+func factorial(n int) int {
+	return factorialIter(1, 1, n)
+}
+
+func factorialIter(product int, counter int, maxCount int) int {
+	if counter > maxCount {
+		return product
+	}
+	return factorialIter(product * counter, counter + 1, maxCount)
+}
+
+func factorialIter1(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return factorialIter1(n-1) * n
+}
+
